7545acc5cdbc8fcd9962a739a88d3229825a0e14
Gourav Kashiv <gouravkashiv3@gmail.com>
2025-10-30 15:43:24 +0530

    fixed hydration error

---
+++ app/_components/reservations/ReservationCard.js
@@ -0,0 +1,33 @@
+"use client";
+import { useState } from "react";
+import ImageCarousel from "./ImageCarousel";
+import ReservationContent from "./ReservationContent";
+import ReservationActions from "./ReservationActions";
+
+function ReservationCard({ booking }) {
+  const { id, startDate, accommodations } = booking;
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const hasMultipleAccommodations = accommodations?.length > 1;
+
+  return (
+    <div className="flex flex-col md:flex-row border border-primary-800">
+      <ImageCarousel
+        accommodations={accommodations}
+        currentImageIndex={currentImageIndex}
+        setCurrentImageIndex={setCurrentImageIndex}
+        hasMultipleAccommodations={hasMultipleAccommodations}
+      />
+
+      <ReservationContent
+        booking={booking}
+        currentImageIndex={currentImageIndex}
+        hasMultipleAccommodations={hasMultipleAccommodations}
+      />
+
+      <ReservationActions bookingId={id} startDate={startDate} />
+    </div>
+  );
+}
+
+export default ReservationCard;
--- app/_components/reservations/ReservationCard/index.js
+++
@@ -1,5 +0,0 @@
-"use client";
-
-import ReservationContent from "./ReservationContent";
-
-export default ReservationContent;
